efed1c904ef47e1c9c402ab9927935f9e2529cc8
invercelina <invercelina@gmail.com>
2024-05-20 10:07:35 +0900

    DropDownMenu 구현

--- app/src/main/java/com/example/carrotcopy/MainActivity.kt
+++ app/src/main/java/com/example/carrotcopy/MainActivity.kt
@@ -4,38 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -90,7 +78,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MainScreen() {
         Column(modifier = Modifier.fillMaxHeight()) {
-            LocalList()
+            LocationDropDown()
             itemList.forEach { item ->
                 CarrotMain(carrotItems = item)
             }
@@ -162,36 +150,32 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun LocalList() {
-        var showList by remember { mutableStateOf(false) }
-        val localPlaces = listOf("Place 1", "Place 2", "Place 3", "Place 4")
-        Box(
-            modifier = Modifier
-                .padding(5.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Box(
-                modifier = Modifier
-                    .border(1.dp, Color.Gray)
-                    .padding(5.dp)
-                    .background(Color.White)
-            ) {
-                Column{
-                    Button(onClick = { showList = !showList }) {
-                        Text(text = if (showList) "Hide Places" else "Show Places")
-                    }
-                    Spacer(modifier = Modifier.height(5.dp))
-                    if (showList) {
-                        localPlaces.forEach { place ->
-                            Text(text = place, modifier = Modifier
-                                .padding(5.dp)
-                                .clickable { /*ToDo*/ })
-                        }
-                    }
-                    Divider(color = Color.Gray)
-                }
+    fun LocationDropDown() {
+        var isexpanded by remember {
+            (mutableStateOf(false))
+        }
+        var currentLocation by remember {
+            mutableStateOf("현재 장소")
+        }
+        Column {
+            Button(onClick = { isexpanded = !isexpanded }) {
+                Text(text = currentLocation)
             }
+            DropdownMenu(
+                expanded = isexpanded,
 
+                onDismissRequest = { isexpanded = false },
+                modifier = Modifier.wrapContentSize(),
+            ) {
+                DropdownMenuItem(text = { Text(text = "역삼동") }, onClick = {
+                    isexpanded = false
+                    currentLocation = "역삼동"
+                })
+                DropdownMenuItem(text = { Text(text = "삼성동") }, onClick = {
+                    isexpanded = false
+                    currentLocation = "삼성동"
+                })
+            }
         }
     }
 }
